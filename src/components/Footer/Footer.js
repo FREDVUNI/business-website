452af76fee6087b670e07ee869d2151412682e19
Fred Vuni <41730664+FREDVUNI@users.noreply.github.com>
2024-02-14 14:47:40 +0300

    make the year dynamic

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -66,7 +66,7 @@ const Footer = () =>{
                         ULTRA
                     </SocialLogo>
                     <WebsiteRights>
-                        ULTRA &copy; 2021
+                        ULTRA &copy; document.write(new Date().getFullYear());
                     </WebsiteRights>
                     <SocialIcons>
                         <SocialIconLink to="/" target="_blank" aria-label="Facebook">
